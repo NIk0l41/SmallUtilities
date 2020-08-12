f137c5a4245645837ec18fb5e05b2aa27f4829b9
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2020-08-13 09:35:03 +1000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Quizlet_Hacks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Quizlet Hacks";
+            bool showMenu = true;
+            while (showMenu) {
+                showMenu = LoadMenu();
+            }
+
+        }
+
+        private static bool LoadMenu() {
+            Console.Clear();
+            Console.WriteLine("Function:");
+            Console.WriteLine("1) Sine Rule");
+            Console.WriteLine("2) Cosine Rule");
+            Console.WriteLine("0) Exit");
+
+            switch (Console.ReadLine()) {
+                case "1":
+                    SineRule();
+                    return true;
+                case "2":
+                    CosineRule();
+                    return true;
+                case "0":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        private static void SineRule() {
+            Console.Clear();
+            Console.WriteLine("Do you want to find:");
+            Console.WriteLine("1) Angle");
+            Console.WriteLine("2) Side Length");
+            Console.WriteLine("0) Nvm, take me back");
+
+            switch (Console.ReadLine()) {
+                case "1":
+                    SineFindAngle();
+                    break;
+                case "2":
+                    SineFindSide();
+                    break;
+                case "0":
+                    break;
+
+            }
+        }
+
+        private static void SineFindAngle() {
+            Console.Clear();
+            Console.Write("Side Length Opposing Angle: ");
+            string _a = Console.ReadLine();
+            Console.Write("Second Side Length: ");
+            string _b = Console.ReadLine();
+            Console.Write("Angle Opposing Second Side Length: ");
+            string _B = Console.ReadLine();
+            double a = Convert.ToDouble(_a);
+            double b = Convert.ToDouble(_b);
+            double B = Convert.ToDouble(_B);
+            double C = Math.PI / 180;
+            double A = C*Math.Asin(((a*(C*Math.Sin(B*C)))/b)*C);
+            Console.WriteLine("Output: B = " + A);
+            Console.ReadLine();
+        }
+
+        private static void SineFindSide() {
+            Console.Clear();
+            Console.Write("Angle Opposing Side Length: ");
+            string _A = Console.ReadLine();
+            Console.Write("Second Side Length: ");
+            string _b = Console.ReadLine();
+            Console.Write("Angle Opposing Second Side Length: ");
+            string _B = Console.ReadLine();
+            double A = Convert.ToDouble(_A);
+            double b = Convert.ToDouble(_b);
+            double B = Convert.ToDouble(_B);
+            double C = Math.PI / 180;
+            double a = ((C*Math.Sin(A*C)*b)/(C*Math.Sin(B*C)));
+            Console.WriteLine("Output: a = " + a);
+            Console.ReadLine();
+
+        }
+
+        private static void CosineRule() {
+            Console.Clear();
+            Console.WriteLine("Do you want to find:");
+            Console.WriteLine("1) Angle");
+            Console.WriteLine("2) Side Length");
+            Console.WriteLine("0) Take me back to the, MENU");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    CosineFindAngle();
+                    break;
+                case "2":
+                    CosineFindSide();
+                    break;
+                case "0":
+                    break;
+
+            }
+        }
+
+        private static void CosineFindAngle() {
+            Console.Write("Side Length Opposing Angle: ");
+            string _c = Console.ReadLine();
+            Console.Write("Second Side Length: ");
+            string _a = Console.ReadLine();
+            Console.Write("Third Side Length: ");
+            string _b = Console.ReadLine();
+            double a = Convert.ToDouble(_a);
+            double b = Convert.ToDouble(_b);
+            double c = Convert.ToDouble(_c);
+            double d = 180 / Math.PI;
+            double p = -(((c*c)-(a*a)-(b*b)) / (2*a*b));
+            double C;
+            C = d * Math.Acos(p);
+            Console.WriteLine("Output: C = " + C);
+            Console.ReadLine();
+        }
+
+        private static void CosineFindSide() {
+            Console.Write("Angle Opposing Side Length: ");
+            string _C = Console.ReadLine();
+            Console.Write("Second Side Length: ");
+            string _a = Console.ReadLine();
+            Console.Write("Third Side Length: ");
+            string _b = Console.ReadLine();
+            double a = Convert.ToDouble(_a);
+            double b = Convert.ToDouble(_b);
+            double C = Convert.ToDouble(_C);
+            double d = Math.PI / 180;
+            double c = Math.Sqrt((a*a) + (b*b) + (-2 * a * b) * Math.Cos(d*C));
+            Console.WriteLine("Output: c = " + c);
+            Console.ReadLine();
+        }
+
+    }
+}
