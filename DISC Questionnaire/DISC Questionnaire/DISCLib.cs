96f980c6714fdeaf3c5e20a90fe319146e1620e6
Nicholas Maver <68896514+NIk0l41@users.noreply.github.com>
2021-05-16 02:57:06 +1000

    DISC Questionnaire
    Didn't know I hadn't pushed this already.

---
+++ DISC Questionnaire/DISC Questionnaire/DISCLib.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DISC_Questionnaire
+{
+    class DiscCalculator{
+
+        int[] most = new int[4];
+        int[] least = new int[4];
+        int[] change = new int[4];
+
+        public string[] Start(int[,] input) {
+            int[] change = { 0, 0, 0, 0 };
+            for (int i = 0; i < 4; i++) {
+                most[i] = input[i, 0];
+                least[i] = input[1, i];
+                change[i] = most[i] - least[i];
+            }
+            string out1 = "1";
+            string out2 = "2";
+            string out3 = "3";
+            string[] out0 = { out1, out2, out3 };
+            return out0;
+        }
+
+        string Mask1(int[] input) {
+            
+        }
+
+    }
+
+    class Personality{
+
+    }
+}
---
+++ DISC Questionnaire/DISC Questionnaire/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DISC Questionnaire")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DISC Questionnaire")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("267bbcb9-27a9-46cd-aa48-e0bae62d746a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
